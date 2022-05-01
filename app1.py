c411394da8cedb36db3f18972a511448d1054c82
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-01 20:41:59 +0630

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 #st.sidebar.write("Setting")
-model=load_model(''neural_networks.h5')
+model=load_model('neural_networks.h5')
 
 st.title("cats and dogs classification",)
 st.header("insert ur  image",)
