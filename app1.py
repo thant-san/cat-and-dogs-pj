c4656948c1f13d0a2ead119fcb9f0c264007a8c0
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-01 20:41:25 +0630

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,11 +2,13 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from PIL import Image,ImageOps
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 #st.sidebar.write("Setting")
+model=load_model(''neural_networks.h5')
 
-
-st.title("brain tumer classification",)
-st.header("insert ur mri image",)
+st.title("cats and dogs classification",)
+st.header("insert ur  image",)
 
 #st.set_option('deprecation.showfileUploaderEncoding', False)
 file_upload=st.file_uploader("choose the mri file",type=['jpg','png','jpeg'])
@@ -17,3 +19,5 @@ image=ImageOps.fit(image,size,Image.ANTIALIAS)
 img=np.asarray(image)
 img_reshape=img[np.newaxis,...]
 st.image(img_reshape, caption='your mri image')
+predict=model.img_reshape
+st.write('predict')
