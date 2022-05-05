a27e6c7f53b8920b6252ebe9b13c9875ba460568
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-05 23:34:03 +0630

    Update app1.py

--- app1.py
+++ app1.py
@@ -23,12 +23,8 @@ prediction=model.predict(img_reshape)
 class_names=['cats','dogs']
 string=class_names[np.argmax(prediction)]
 st.success(string)
-st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
-expander = st.expander("See explanation")
-expander.write("""
-     The chart above shows some numbers I picked for you.
-     I rolled actual dice for these, so they're *guaranteed* to
-     be random.
- """)
-expander.image("https://static.streamlit.io/examples/dice.jpg")
+
+expander = st.beta_expander('see more',expanded=True)
+with expnder:
+     st.info("hi i m")
